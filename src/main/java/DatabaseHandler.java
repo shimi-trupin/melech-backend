53a8c96bc54c6ce70ab0e19b9fe104f7b8019919
alex-borochov <alex.borochov@gmail.com>
2017-09-26 13:59:37 +0300

    Added initialization

--- src/main/java/DatabaseHandler.java
+++ src/main/java/DatabaseHandler.java
@@ -1,3 +1,4 @@
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 
 
@@ -7,6 +8,17 @@ public class DatabaseHandler {
 
     public DatabaseHandler() {
 
+    }
+
+    public void initialize(){
+        String host = "192.168.0.67";
+        int port = 27017;
+        String databaseName = "melechdb";
+
+        MongoClient mongo = new MongoClient(host, port);
+        // Accessing the database
+        m_database= mongo.getDatabase(databaseName);
+        System.out.println("Connected to database successfully");
     }
 
 }
