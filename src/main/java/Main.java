7ea8ca12c1b9fbb4545f433cc22498bb96c5257f
alex-borochov <alex.borochov@gmail.com>
2017-09-26 12:17:18 +0300

    Added a simple hello-world GET request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,10 @@
  * Created by shimi on 26/09/2017.
  * just for test
  */
+import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        get("/hello", (req, res) -> "Hello World");
     }
 }
