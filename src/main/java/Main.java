cb7848b7858fb13e9728755ee29ceccf1cddc1f7
shimi <shimon.trupin1994@gmail.com>
2017-09-26 09:25:41 +0300

    initial commit - test

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by shimi on 26/09/2017.
+ * just for test
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
